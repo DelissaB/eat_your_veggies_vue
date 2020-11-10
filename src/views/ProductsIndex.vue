29d1d4ea8e168a21425c083725d4ec3a9a121192
DelissaB <delissacbeaman@gmail.com>
2020-11-10 15:15:59 -0600

    first commit

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="products">
+  <h1>All Products</h1>
+  <div v-for="product in products">
+    <!-- <img 
+    v-bin:src="product.url"
+    v-bind:alt="product.name"/> -->
+    <h2>
+      {{ product.name }}
+      </h2>
+      <p>Description:
+      {{ product.description }}
+      </p>
+      <p>General Use:
+      {{ product.general_use }}
+      </p>
+      <p>Price:
+      {{ product.price }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      products: [],
+    };
+  },
+      created: function() {
+        axios.get("/api/products")
+    .then(response => {
+      console.log("products index", response);
+      this.products = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
+
